c5eb6c3fe1d82c2f5c80096e2dc758057ba5ac7d
kumpulanomg-ux <kumpulanomg@gmail.com>
2025-08-29 22:12:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -17,14 +17,9 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
 import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
 
 class Config:
-    def __init__(self) -> None:
+    def __init__(self) -> None:   # ✅ تعديل هنا
         self.API_ID: str = os.environ.get("API_ID", None)
         self.API_HASH: str = os.environ.get("API_HASH", None)
         self.SESSION: str = os.environ.get("SESSION", None)
@@ -35,10 +30,11 @@ class Config:
         if not self.SESSION or not self.API_ID or not self.API_HASH:
             print("ERROR: SESSION, API_ID and API_HASH is required!")
             quit(0)
+
         self.SPOTIFY: bool = False
         self.QUALITY: str = os.environ.get("QUALITY", "high").lower()
-        self.PREFIXES: list = os.environ.get("PREFIX", "!").split()
-        self.LANGUAGE: str = os.environ.get("LANGUAGE", "en").lower()
+        self.PREFIXES: list = os.environ.get("PREFIX", ".").split()
+        self.LANGUAGE: str = os.environ.get("LANGUAGE", "ar").lower()
         self.STREAM_MODE: str = (
             "audio"
             if (os.environ.get("STREAM_MODE", "audio").lower() == "audio")
@@ -49,4 +45,4 @@ class Config:
         self.SPOTIFY_CLIENT_SECRET: str = os.environ.get("SPOTIFY_CLIENT_SECRET", None)
 
 
-config = Config()
+config = Config()   # ✅ هنا بيستدعي الكلاس
