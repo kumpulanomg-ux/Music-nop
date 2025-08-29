9b02cc6f5951cdb533df819b07d067e62a22320e
kumpulanomg-ux <kumpulanomg@gmail.com>
2025-08-29 21:56:52 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ echo ">> FETCHING UPSTREAM..."
 git clone https://github.com/AsmSafone/MusicPlayer /MusicPlayer
 
 echo ">> INSTALLING REQUIREMENTS..."
-cd /MusicPlayer
+cd MusicPlayer
 pip3 install -U -r requirements.txt
 
 echo ">> STARTING MUSIC PLAYER USERBOT..."
